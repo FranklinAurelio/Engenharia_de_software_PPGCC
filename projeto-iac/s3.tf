bbc5c00afc985ee718b3f04571173c6683bc87b0
Victor Moraes <victor.moraes@outlook.com.br>
2024-04-21 16:46:33 -0300

    Criar configuração terraform para upload de arquivos do projeto frontend para o aws s3

--- projeto-iac/s3.tf
+++ projeto-iac/s3.tf
@@ -16,3 +16,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption_con
     }
   }
 }
+
+resource "aws_s3_object" "app_frontend_objects" {
+  for_each = fileset("../projeto-frontend/", "**")
+  bucket   = aws_s3_bucket.s3_bucket.id
+  key      = each.value
+  source   = "../projeto-frontend/${each.value}"
+  etag     = filemd5("../projeto-frontend/${each.value}")
+
+  tags = {
+    gerenciado-por = "terraform"
+  }
+}
