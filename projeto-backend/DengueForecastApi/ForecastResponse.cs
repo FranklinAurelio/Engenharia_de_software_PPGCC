125794a6320bf3b9fd404dd4100cd50633a840b4
Victor Moraes <victor.moraes@outlook.com.br>
2024-06-14 12:05:27 -0300

    Atualizar api .net

---
+++ projeto-backend/DengueForecastApi/ForecastResponse.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+
+namespace DengueForecastApi
+{
+    public class ForecastResponse
+    {
+        public List<Historical> Historical { get; set; }
+
+        public List<Prediction> Prediction { get; set; }
+
+        public List<Forecast> Forecast { get; set; }
+    }
+
+    public class Historical
+    {
+        public string? MesAno { get; set; }
+
+        public string? PrevisaoCasos { get; set; }
+
+        public string? PrevisaoMinima { get; set; }
+
+        public string? PrevisaoMaxima { get; set; }
+    }
+
+    public class Prediction
+    {
+        public string? MesAno { get; set; }
+
+        public string? PrevisaoCasos { get; set; }
+
+        public string? PrevisaoMinima { get; set; }
+
+        public string? PrevisaoMaxima { get; set; }
+
+        public string? VariacaoPercentual { get; set; }
+    }
+
+    public class Forecast
+    {
+        public string? MesAno { get; set; }
+
+        public string? PrevisaoCasos { get; set; }
+
+        public string? PrevisaoMinima { get; set; }
+
+        public string? PrevisaoMaxima { get; set; }
+    }
+
+    public class Response
+    {
+        [JsonProperty("statusCode")]
+        public int statusCode { get; set; }
+
+        [JsonProperty("body")]
+        public List<ForecastResponse> body { get; set; }
+
+        [JsonProperty("headers")]
+        public object? headers { get; set; }
+    }
+}
--- projeto-backend/DengueForecastApi/Program.cs
+++ projeto-backend/DengueForecastApi/Program.cs
@@ -1,6 +1,12 @@
 using DengueForecastApi;
+using Microsoft.AspNetCore.Http;
 using MiniValidation;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,39 +28,23 @@ app.MapPost("/obter-forecast", async (ModeloRequest modeloRequest) =>
     if (!MiniValidator.TryValidate(modeloRequest, out var errors))
         return Results.ValidationProblem(errors);
 
-    var requestUri = "https://vgl6enaghduhpbmk7ga6xicr5u0ddpfa.lambda-url.us-east-1.on.aws/";
+    var requestUri = "https://ew4n1xndv4.execute-api.us-east-1.amazonaws.com/dev/";
     var httpClient = new HttpClient();
-    var stringContent = new StringContent(modeloRequest.ToString(), Encoding.UTF8, "application/json");
-    //var result = await httpClient.PostAsync(requestUri, stringContent);
-    //var forecast = result.Content.ReadAsStringAsync().Result ?? string.Empty;
+    var json = JsonConvert.SerializeObject(modeloRequest);
+    var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
 
-    var response = GerarResponseMock();
+    var response = await httpClient.PostAsync(requestUri, stringContent);
+    response.EnsureSuccessStatusCode();
+    var responseString = await response.Content.ReadAsStringAsync();
+    var responseObject = JsonConvert.DeserializeObject<Response>(responseString);
 
-    return response.Any()
-        ? Results.Ok(response)
+    return true
+        ? Results.Ok(responseString)
         : Results.NotFound("Nenhum resultado obtido.");
 })
     .ProducesValidationProblem()
-    .Produces<List<ModeloResponse>>(StatusCodes.Status200OK)
+    .Produces<List<ForecastResponse>>(StatusCodes.Status200OK)
     .Produces(StatusCodes.Status404NotFound)
     .WithName("DengueForecast");
 
 app.Run();
-
-List<ModeloResponse> GerarResponseMock()
-{
-    Random random = new Random();
-
-    var response = new List<ModeloResponse>();
-    var quantidadeIteracoes = random.Next(100, 200);
-
-    var data = new DateTime(2024, 7, 1);
-    var range = (DateTime.Today - data).Days * -1;
-
-    for (int i = 0; i < quantidadeIteracoes; i++)
-    {
-        response.Add(new ModeloResponse(random.Next(1, 100), data.AddDays(random.Next(range)).ToString("MM/yyyy")));
-    }
-
-    return response;
-}
